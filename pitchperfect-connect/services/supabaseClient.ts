595d4194fd055641446d20ca605fc1ef685999e4
LumioPhotography <hello@lumiophotography.co.uk>
2025-12-11 00:35:17 +0000

    Add files via upload

--- pitchperfect-connect/services/supabaseClient.ts
+++ pitchperfect-connect/services/supabaseClient.ts
@@ -12,7 +12,7 @@ import { createClient } from '@supabase/supabase-js';
 // ------------------------------------------------------------------
 
 // REPLACE THE TEXT INSIDE THE QUOTES BELOW:
-const SUPABASE_URL = 'https://YOUR_PROJECT_ID.supabase.co'; 
-const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY_HERE';
+const SUPABASE_URL = 'https://xajvdlneavikqgfflezc.supabase.co'; 
+const SUPABASE_ANON_KEY = 'sb_publishable_TkSBRoU2C8tECbCMcYLPaw_glCv7VBM';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
